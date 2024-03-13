725702c947c3826a05779737df1be1d8f17de73f
Miguel Risco-Castillo <mriscoc@users.noreply.github.com>
2024-03-13 16:43:57 -0500

    Added Acquisition software demo for the DDC264EVM_IO DLL

---
+++ DemoCapture/DDCAcquisition.cs
@@ -0,0 +1,222 @@
+﻿/**
+ * Acquisition software demo for the DDC264EVM_IO DLL
+ * https://www.ti.com/tool/DDC264EVM
+ *
+ * Author: Miguel Risco-Castillo
+ * Version: 3.2
+ * Date: 2024/02/27
+ *
+ * LICENSE: MIT License.
+ */
+
+using System.Diagnostics;
+using System.Text;
+
+partial class Program
+{
+
+    string FPGARegStr(int RegN)
+    {
+        StringBuilder TempStr = new(61);
+        EVM_RegNameTable(RegN, TempStr, 61);
+        return TempStr.ToString();
+    }
+
+    int EVM_RegDataOut(int Reg, int Data)
+    {
+        int resOut = EVM_RegDataOut(ref USBdev, ref Reg, ref Data);
+        if (resOut != 0)
+        {
+            string RegS = Reg.ToString("X");
+            Console.WriteLine($"Error writting FPGA register: 0x{RegS} {FPGARegStr(Reg)}");
+        }
+        return resOut;
+    }
+
+    bool EVM_Prepare()
+    {
+        bool resOut = EVM_ResetDDC(ref USBdev);
+        if (!resOut)
+        {
+            Console.WriteLine($"Error when resetting DDC");
+            return false;
+        }
+        resOut = EVM_ClearTriggers(ref USBdev);
+        if (!resOut)
+        {
+            Console.WriteLine("Error when clearing registers");
+            return false;
+        }
+        resOut = EVM_DataSequence(ref USBdev, ref CFGHIGH, ref CFGLOW);
+        if (!resOut) Console.WriteLine("Error when writting dataSequence");
+
+        return resOut;
+    }
+
+    void Reset_Regs()
+    {
+        for (int i = 0; i<regsSize; i++) RegsEnable[i] = 0;
+    }
+
+    void Set_RegIn(int reg, int val)
+    {
+        RegsIn[reg] = val & 0xFF;
+        RegsEnable[reg] = 1;
+    }
+
+    void Set_Regs()
+    {
+        int channelValue = CHANNEL_COUNT switch
+        {
+            <= 1 => 0,
+            <= 2 => 1,
+            <= 4 => 2,
+            <= 8 => 3,
+            <= 16 => 4,
+            <= 32 => 5,
+            <= 64 => 6,
+            <= 128 => 7,
+            <= 256 => 8,
+            _ => 100,
+        };
+
+        if (channelValue == 100)
+        {
+            Console.WriteLine("Invalid channel count");
+            Environment.Exit(-1);
+        }
+
+        else CHANNEL_COUNT = (int)Math.Pow(2, channelValue);
+        // CONV
+        Set_RegIn(0x01, CONV_LOW_INT - 1 >> 16);
+        Set_RegIn(0x02, CONV_LOW_INT - 1 >> 8);
+        Set_RegIn(0x03, CONV_LOW_INT - 1);
+        Set_RegIn(0x04, CONV_HIGH_INT - 1 >> 16);
+        Set_RegIn(0x05, CONV_HIGH_INT - 1 >> 8);
+        Set_RegIn(0x06, CONV_HIGH_INT - 1);
+
+        // DDC Sys Clock
+        Set_RegIn(0x07, (CLC_HIGH << 4) | (CLC_LOW & 0x0F));
+        Set_RegIn(0x08, DDC_CLK_CONFIG);
+
+        // Format and Channel Count
+        int FORMAT = CFGHIGH & 1;
+        Set_RegIn(0x09, (FORMAT << 4) + channelValue);
+
+        // DDC Data Clock
+        Set_RegIn(0x0A, (DCLK_HIGH << 4) | (DCLK_LOW & 0x0F));
+        Set_RegIn(0x0B, DCLK_CONFIG);
+
+        // nDVALIDS
+        Set_RegIn(0x0C, NDVALID_IGNORE);
+        Set_RegIn(0x0D, NDVALID_READ);
+        Set_RegIn(0x0E, NDVALID_READ >> 8);
+        Set_RegIn(0x0F, NDVALID_READ >> 16);
+
+        // DCLK Wait
+        Set_RegIn(0x13, DCLK_WAIT_MCLK >> 8);
+        Set_RegIn(0x14, DCLK_WAIT_MCLK);
+
+        //// Format
+        Set_RegIn(0x1F, FORMAT);
+
+        // CLK CFG
+        Set_RegIn(0x20, (CLK_CFG_HI << 4) | (CLK_CFG_LO & 0x0F));
+
+        // CONV Config 
+        Set_RegIn(0x57, CONV_CONFIG); // 0-Freerun, 1-Freerun, 2-Low, 3-High
+
+        // Aditional undocummented registers
+        Set_RegIn(0x51, CONV_WAIT_LOW >> 8);
+        Set_RegIn(0x52, CONV_WAIT_LOW);
+        Set_RegIn(0x53, CONV_WAIT_HIGH >> 8);
+        Set_RegIn(0x54, CONV_WAIT_HIGH);
+        Set_RegIn(0xEB, CLKDELAY_AROUND_CONV);
+    }
+
+    bool EVM_ShowRegisters()
+    {
+        int ErrorFlag = -1;
+        Console.WriteLine("Reading current register values");
+        try
+        {
+            ErrorFlag = EVM_RegsTransfer(ref USBdev, ref RegsIn[0], ref RegsEnable[0], ref RegsOut[0]);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Code exception: " + ex.Message);
+        }
+
+        if (ErrorFlag == 0)
+        {
+            // Print EVM Firmware version
+            Console.WriteLine($"Firmware version: {RegsOut[0x5E] * 256 + RegsOut[0x5F]}");
+            for (int i = 1; i < regsSize; i++) Console.WriteLine($"Register {FPGARegStr(i)} [0x{i:X2}] : 0x{RegsOut[i]:X2}");
+            return true;
+        }
+        else Console.WriteLine("Board is not connected or error in EVM_RegsTransfer, result: " + ErrorFlag);
+        return false;
+    }
+
+    /*
+    From the DDC264EVM Datasheet:
+
+    1. (CHANNEL_COUNT) * (2) * (NDVALID_READ) < 1048576; and
+    2. (CHANNEL_COUNT) * (2) * (NDVALID_READ) / 131072 must result as an integer. 
+
+    Example: 32 channels
+
+    1. 32 x 2 x (NDVALID_READ) < 1048576 -> (NDVALID_READ) < 16384, so 16383 meets this condition
+    2. 32 x 2 x (NDVALID_READ) / 131072 must result as an integer -> 64x16383/131072 = 7.9995...
+
+    So, the more close result that meet the condition 2 is "7" then 32 x 2 x (NDVALID_READ) / 131072 = 7
+    Finally, the maximum NDVALID_READ is 14336 for 32 channels
+
+    Also NDVALID_READ must be even
+
+    */
+
+    void DDC_Adquisition(ref double AllData)
+    {
+        if (!EVM_ShowRegisters()) Environment.Exit(0);
+
+        // Prepare EVM board
+        if (!EVM_Prepare()) Environment.Exit(-1);
+
+        // Clear RegsEnable
+        Reset_Regs();
+
+        // Load configuration into registers
+        Set_Regs();
+
+        try
+        {
+            int ErrorFlag = EVM_RegsTransfer(ref USBdev, ref RegsIn[0], ref RegsEnable[0], ref RegsOut[0]);
+            if (ErrorFlag == 0)
+            {
+                Console.WriteLine("FPGA registers written");
+            }
+            else
+            {
+                Console.WriteLine("Error in EVM_RegsTransfer, result: " + ErrorFlag);
+                Environment.Exit(-1);
+            }
+
+            int AllDataAorBfirst = 0;
+            ErrorFlag = EVM_DataCap(ref USBdev, CHANNEL_COUNT, NDVALID_READ, ref AllData, ref AllDataAorBfirst);
+            if (ErrorFlag != 0)
+            {
+                // If there was an error in EVM_DataCap, display the error code
+                Console.WriteLine("Error in data capture: " + ErrorFlag);
+                Environment.Exit(-1);
+            }
+
+            Console.WriteLine($"Data captured\n");
+
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Code exception: " + ex.Message);
+        }
+    }
+}
---
+++ DemoCapture/Program.cs
@@ -0,0 +1,93 @@
+﻿/**
+ * Acquisition software demo for the DDC264EVM_IO DLL
+ * https://www.ti.com/tool/DDC264EVM
+ *
+ * Author: Miguel Risco-Castillo
+ * Version: 3.2
+ * Date: 2024/02/27
+ *
+ * LICENSE: MIT License.
+ */
+
+using System.Text;
+
+partial class Program
+{
+    private const string dllFile = "DDC264EVM_IO.dll";
+    private const string dataFile = "EVMdata.csv";
+
+    // Configuration Registers
+    int CONV_LOW_INT = 1600;
+    int CONV_HIGH_INT = 1600;
+    int CONV_CONFIG = 0; // 0-Freerun, 1-Freerun, 2-Low, 3-High
+    const int CLC_HIGH = 7;
+    const int CLC_LOW = 7;
+    const int DDC_CLK_CONFIG = 1; // 1=Running, 0=disabled;
+    int CHANNEL_COUNT = 16;
+    int NDVALID_IGNORE = 255;
+    int NDVALID_READ = 256;
+    const int DCLK_HIGH = 0;
+    const int DCLK_LOW = 0;
+    const int DCLK_CONFIG = 1; // 1=Running, 0=disabled
+    const int DCLK_WAIT_MCLK = 13000;
+    const int CLK_CFG_HI = 3;
+    const int CLK_CFG_LO = 3;
+    const int CONV_WAIT_LOW = 1550;
+    const int CONV_WAIT_HIGH = 1550;
+    const int CLKDELAY_AROUND_CONV = 0;
+
+    //// DDC Configuration Register bits
+    //const int DDCbit13 = 0; // CLKDIV
+    //const int DDCbit10 = 1; // Range[1]
+    //const int DDCbit9 = 1; // Range[0]
+    //const int DDCbit8 = 1; // Format: 0=16 bits, 1=20 bits
+    //const int DDCbit7 = 0; // SPEED
+    //const int DDCbit4 = 0; // SLEW
+    //const int DDCbit0 = 0; // TEST
+    //byte CFGLOW = (DDCbit7 << 7) + (DDCbit4 << 4) + DDCbit0;
+    //byte CFGHIGH = (DDCbit13 << 5) + (DDCbit10 << 2) + (DDCbit9 << 1) + DDCbit8;
+    byte CFGLOW = 0;
+    byte CFGHIGH = 7; // Range b11, 20 bits
+
+    int USBdev = 0; // DDC EVM board to send/receive data to/from
+
+    // Register arrays
+    static int regsSize = 255;
+    int[] RegsIn = new int[regsSize];
+    int[] RegsOut = new int[regsSize];
+    int[] RegsEnable = new int[regsSize];
+
+    static void Main()
+    {
+
+        Program program = new();
+        Console.WriteLine("\nAcquisition software demo for the DDC264EVM_IO DLL Version: 3.2\n");
+
+        try // Check if the DLL can be loaded
+        {
+            StringBuilder tempStr = new(100);
+            dllID(tempStr, 100);
+            Console.WriteLine("DLL id: " + tempStr);
+            dllCprght(tempStr, 100);
+            Console.WriteLine(tempStr);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+            System.Environment.Exit(-1);
+        }
+
+        // Initialize the array to store values
+        double[] AllData = new double[program.CHANNEL_COUNT * 2 * program.NDVALID_READ];
+
+        program.DDC_Adquisition(ref AllData[0]);
+
+        // Show captured data
+        for (int i = 0; i < program.NDVALID_READ; i++)
+        {
+            Console.WriteLine($"AllData[{i}] = {AllData[i]}");
+        }
+
+    }
+
+}
